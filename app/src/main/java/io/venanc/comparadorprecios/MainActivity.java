0e60711ca19119461df0d162d82303b99132172e
Alfonso Venancio <alfonso.venancio22@gmail.com>
2019-04-17 20:37:55 -0500

    Rows add and deletion

--- app/src/main/java/io/venanc/comparadorprecios/MainActivity.java
+++ app/src/main/java/io/venanc/comparadorprecios/MainActivity.java
@@ -26,8 +26,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-
     String[] arrayUnitWeights = {"gramos", "kilogramos"};
     String[] arrayUnitVolumes = {"mililitros", "litros"};
     String[] arrayTypes = {"Peso", "Volumen", "Por unidad"};
@@ -35,125 +33,125 @@ public class MainActivity extends AppCompatActivity {
     private TableLayout tableOptions;
     private int idSet;
     private float screenDensity;
-
+    private List<Spinner> spinnerList;
+    private List<EditText> quantitiesList;
+    private List<EditText> pricesList;
+    private Spinner spinnerTypes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Spinner spinnerTypes = findViewById(R.id.spinnerType);
-        final Spinner spinnerUnits = findViewById(R.id.spinnerUnit);
-        final Spinner spinnerTest = findViewById(R.id.spinnerPrueba);
-        final Button buttonPlus = findViewById(R.id.button);
-        final Button buttonMinus = findViewById(R.id.button2);
-        tableOptions = findViewById(R.id.tableOptions);
-        rowsOptions  = new ArrayList<>();
-        idSet = 1;
-        screenDensity = getApplicationContext().getResources().getDisplayMetrics().density;
+        //INIT OF ALL VIEWS
         final TableRow firstRow = findViewById(R.id.firstRow);
         final TableRow secondRow = findViewById(R.id.secondRow);
+        final Spinner spinnerTypes = findViewById(R.id.spinnerType);
+        final Spinner firstSpinner = findViewById(R.id.firstSpinner);
+        final Spinner secondSpinner = findViewById(R.id.secondSpinner);
+        final Button buttonPlus = findViewById(R.id.button);
+        final Button buttonMinus = findViewById(R.id.button2);
+        //INIT OF ALL VARIABLES
+        screenDensity = getApplicationContext().getResources().getDisplayMetrics().density;
+        idSet = 1;
+        //LISTS
+        rowsOptions  = new ArrayList<>();
         firstRow.setId(idSet++);
         secondRow.setId(idSet++);
         rowsOptions.add(firstRow);
         rowsOptions.add(secondRow);
-        //Poblate of the types spinner
-        poblateSpinner(spinnerTypes,arrayTypes);
-        //Types Spinners on selected item
+        quantitiesList = new ArrayList<EditText>();
+        spinnerList = new ArrayList<Spinner>();
+        spinnerList.add(firstSpinner);
+        spinnerList.add(secondSpinner);
+        pricesList = new ArrayList<EditText>();
+        tableOptions = findViewById(R.id.tableOptions);
+        populateSpinner(spinnerTypes,arrayTypes);
         spinnerTypes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 switch ((String)spinnerTypes.getSelectedItem()){
                     case "Peso":
-                        poblateSpinner(spinnerUnits, arrayUnitWeights);
-                        poblateSpinner(spinnerTest,arrayUnitWeights);
-                        spinnerUnits.setEnabled(true);
+                        populateAllSpinners(spinnerList, arrayUnitWeights);
                         break;
                     case "Volumen":
-                        poblateSpinner(spinnerUnits, arrayUnitVolumes);
-                        poblateSpinner(spinnerTest,arrayUnitVolumes);
-                        spinnerUnits.setEnabled(true);
+                        populateAllSpinners(spinnerList, arrayUnitVolumes);
                         break;
                     default:
                         Snackbar.make(findViewById(R.id.main_layout),"Todavia no esta soportado :(",Snackbar.LENGTH_SHORT).show();
-                        spinnerUnits.setEnabled(false);
                 }
             }
-
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
                 Snackbar.make(findViewById(R.id.main_layout),"Nada seleccionado",Snackbar.LENGTH_SHORT);
             }
         });
-
         buttonPlus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 addRow();
+                Spinner spinnerTopopulate = spinnerList.get(spinnerList.size()-1);
+                switch ((String)spinnerTypes.getSelectedItem()){
+                    case "Peso":
+                        populateSpinner(spinnerTopopulate,arrayUnitWeights);
+                        break;
+                    case "Volumen":
+                        populateSpinner(spinnerTopopulate,arrayUnitVolumes);
+                        break;
+                    default:
+                        Snackbar.make(findViewById(R.id.main_layout),"Todavia no esta soportado :(",Snackbar.LENGTH_SHORT).show();
+                }
             }
         });
-
         buttonMinus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 eraseRow();
             }
         });
-
     }
 
-    protected void poblateSpinner(Spinner spinnerToPoblate, String[] arrayToUse){
+    protected void populateSpinner(Spinner spinnerToPopulate, String[] arrayToUse){
         List<String> listAux = Arrays.asList(arrayToUse);
         ArrayAdapter<String> adapterAux = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, listAux);
         adapterAux.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerToPoblate.setAdapter(adapterAux);
+        spinnerToPopulate.setAdapter(adapterAux);
     }
 
     protected void addRow(){
         TableRow rowAux = new TableRow(this);
-        rowAux.setId(idSet);
-        EditText editAux = new EditText(this);
+        EditText editQuantityAux = new EditText(this);
         Spinner spinnerAux = new Spinner(this);
         TextView textAux =  new TextView(this);
-        EditText editAux2 = new EditText(this);
-        textAux.setText(String.valueOf(idSet++));
-
+        EditText editPriceAux = new EditText(this);
+        //Attributes of the whole row
+        rowAux.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        rowAux.setGravity(Gravity.CENTER);
+        rowAux.setId(idSet++);
         //Attributes of the first EditText of each row
-        editAux.setText("1");
-        editAux.setGravity(Gravity.CENTER);
-        editAux.setEms(2);
-        editAux.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
-        editAux.setFilters(new InputFilter[]{
-                new InputFilter.LengthFilter(4)
-        });
-
+        editQuantityAux.setText("1");
+        editQuantityAux.setGravity(Gravity.CENTER);
+        editQuantityAux.setEms(2);
+        editQuantityAux.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        editQuantityAux.setFilters(new InputFilter[]{new InputFilter.LengthFilter(4)});
+        quantitiesList.add(editQuantityAux);
         //Attributes of the Spinner of each row
         spinnerAux.setPadding((int)(10*screenDensity),0,0,0);
         spinnerAux.setGravity(Gravity.CENTER);
-        //spinnerAux.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.MATCH_PARENT));
-
+        spinnerList.add(spinnerAux);
         //Attributes of the TextView of each row
         textAux.setText("$");
-        //textAux.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT));
         textAux.setPadding((int)(10*screenDensity),0,0,0);
         textAux.setTextSize(15);
         textAux.setTextColor(Color.BLACK);
-
         //Attributes of the second EditText of each row
-        editAux2.setText("Precio");
-        editAux2.setGravity(Gravity.CENTER);
-        editAux2.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
-        editAux2.setFilters(new InputFilter[]{
-                new InputFilter.LengthFilter(9)
-        });
-
-
-
-        rowAux.setGravity(Gravity.CENTER);
-        rowAux.addView(editAux);
-
+        editPriceAux.setText("Precio");
+        editPriceAux.setGravity(Gravity.CENTER);
+        editPriceAux.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        editPriceAux.setFilters(new InputFilter[]{new InputFilter.LengthFilter(9)});
+        pricesList.add(editPriceAux);
+        rowAux.addView(editQuantityAux);
         rowAux.addView(spinnerAux);
         rowAux.addView(textAux);
-        rowAux.addView(editAux2);
-
+        rowAux.addView(editPriceAux);
         rowsOptions.add(rowAux);
         tableOptions.addView(rowAux);
     }
@@ -166,6 +164,12 @@ public class MainActivity extends AppCompatActivity {
         rowsOptions.remove(rowsOptions.size()-1);
         tableOptions.removeView(findViewById(--idSet));
         return true;
+    }
+
+    protected void populateAllSpinners(List<Spinner> spinnerList, String[] arrayToUse){
+        for(Spinner spinnerOption : spinnerList){
+            populateSpinner(spinnerOption, arrayToUse);
+        }
     }
 
 }
